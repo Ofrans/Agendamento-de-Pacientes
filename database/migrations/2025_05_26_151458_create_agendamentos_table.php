1cde281a5032f53f5b9170859a115b52b5e67e4e
frans <fransmofilho@gmail.com>
2025-05-26 14:40:27 -0300

    Tabelas do banco criadas

---
+++ database/migrations/2025_05_26_151458_create_agendamentos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('agendamentos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->date('data');
+            $table->time('hora');
+            $table->unsignedBigInteger('paciente_id');
+            $table->unsignedBigInteger('medico_id');
+            $table->foreign('paciente_id')->references('id')->on('pacientes')->onDelete('cascade');
+            $table->foreign('medico_id')->references('id')->on('medicos')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('agendamentos');
+    }
+};
